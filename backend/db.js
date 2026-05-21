68aa56d023584b7ccd868bee1e351036739570aa
iguirmiam1 <iguirmia.mustapha@gmail.com>
2026-05-21 10:06:28 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL, // ✅ lit la variable Render
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.on('error', (err) => {
